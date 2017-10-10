467ce6b0558b11d0708d1ac78ddcae90ee40c1af
VoMinhHoang <fanxico.vo96@gmail.com>
2017-10-10 14:23:58 +0700

    Client - Server

---
+++ VoMinhHoang_Buoi2/Server/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        Socket client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 12345);
+            server.Bind(ip);
+            server.Listen(5);
+            client = server.Accept();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(textBox2.Text);
+            client.Send(data);
+            listBox1.Items.Add("Server: " + textBox2.Text);
+            textBox2.Text = "";
+
+            data = new byte[1024];
+            client.Receive(data);
+            listBox1.Items.Add("Client: " + Encoding.ASCII.GetString(data));
+        }
+    }
+}
